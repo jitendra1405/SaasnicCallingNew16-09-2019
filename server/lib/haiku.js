3a61efda6f0ae36bd7ea3d513815eea3a4ad1a36
jitendra1405 <53037241+jitendra1405@users.noreply.github.com>
2019-09-16 17:42:26 +0530

    Update haiku.js

--- server/lib/haiku.js
+++ server/lib/haiku.js
@@ -22,43 +22,14 @@ const nouns = [
   'frog', 'smoke', 'star'
 ];
 var someVar = [];
-var http = require("http");
-var express = require('express');
-var app = express();
-var bodyParser = require('body-parser');
-var urlencodedParser = bodyParser.urlencoded({ extended: true });
- 
-// Running Server Details.
-var server = app.listen(8082, function () {
-  var host = server.address().address
-  var port = server.address().port
-  console.log("Example app listening at %s:%s Port", host, port)
+
+const bodyParser= require('body-parser');
+app.use(bodyParser.urlencoded({extended:true}));
+app.post('/',(req,res)=>{
+
+let firstName = req.body.firstName; // here you can get the value of from the textbox
+
 });
- 
- 
-app.get('/form', function (req, res) {
-  var html='';
-  html +="<body>";
-  html += "<form action='/thank'  method='post' name='form1'>";
-  html += "Name:</p><input type= 'text' name='name'>";
-  html += "Email:</p><input type='text' name='email'>";
-  html += "address:</p><input type='text' name='address'>";
-  html += "Mobile number:</p><input type='text' name='mobilno'>";
-  html += "<input type='submit' value='submit'>";
-  html += "<INPUT type='reset'  value='reset'>";
-  html += "</form>";
-  html += "</body>";
-  res.send(html);
-});
- 
-app.post('/thank', urlencodedParser, function (req, res){
-  var reply='';
-  reply += "Your name is" + req.body.name;
-  reply += "Your E-mail id is" + req.body.email; 
-  reply += "Your address is" + req.body.address;
-  reply += "Your mobile number is" + req.body.mobilno;
-  res.send(reply);
- });
 
 
 var mysql = require('mysql');  
