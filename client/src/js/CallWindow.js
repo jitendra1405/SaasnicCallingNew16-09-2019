6a4bb93b356c62c3337f377601916af6ea786018
jitendra1405 <53037241+jitendra1405@users.noreply.github.com>
2019-09-16 12:59:36 +0530

    Update CallWindow.js

--- client/src/js/CallWindow.js
+++ client/src/js/CallWindow.js
@@ -213,20 +213,7 @@ document.getElementById("myBtn").disabled = false;
     );
   }
 }
-var mysql = require('mysql');  
-var con = mysql.createConnection({  
-  host     : 'sql12.freemysqlhosting.net',
-    database : 'sql12304794',
-    user     : 'sql12304794',
-    password : 'PLSEEGHnWv', 
-});  
-con.connect(function(err) {  
-if (err) throw err;  
-con.query("SELECT * FROM user", function (err, result) {  
-if (err) throw err;  
-console.log(result);  
-});  
-});  
+
 
 CallWindow.propTypes = {
   status: PropTypes.string.isRequired,
