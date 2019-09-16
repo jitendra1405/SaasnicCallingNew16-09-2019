f0c65195e2b33e2af414eb34fedb917edd5e8d51
jitendra1405 <53037241+jitendra1405@users.noreply.github.com>
2019-09-16 12:33:28 +0530

    Update main.js

--- server/main.js
+++ server/main.js
@@ -4,3 +4,7 @@ const server = require('./lib/server');
 config.PORT = process.env.PORT || config.PORT;
 
 server.run(config);
+
+var test_connection = server.connection.query('SELECT * FROM `test`', function (error, results, fields) {
+  console.log(results);
+});
