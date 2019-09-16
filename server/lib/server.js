5e70947afece0a1c3d44e8f7394364b7f5cd3060
jitendra1405 <53037241+jitendra1405@users.noreply.github.com>
2019-09-16 13:47:14 +0530

    Update server.js

--- server/lib/server.js
+++ server/lib/server.js
@@ -4,40 +4,13 @@ const socket = require('./socket');
 
 const app = express();
 const server = createServer(app);
-var mysql = require('mysql');
+
 
 
 app.use('/', express.static(`${process.cwd()}/../client`));
-var db_config = {
-    host     : 'sql12.freemysqlhosting.net',
-    database : 'sql12304794',
-    user     : 'sql12304794',
-    password : 'PLSEEGHnWv'
-};
 
-var connection;
-
-connection = mysql.createConnection(db_config);
-app.get('/', function(request, response) {
-    connection.query('SELECT * from user', function(err, rows, fields) {
-        if (err) {
-            console.log('error: ', err);
-            throw err;
-        }
-	    
-        response.send(['Hello World!!!! HOLA MUNDO!!!!', rows]);
-    });
-});
-
-
-
-
-
- 
 module.exports.run = (config) => {
   server.listen(config.PORT);
   socket(server);
-  app.listen(port, function() {
-    console.log("Listening on " + port);
   console.log(`Server is listening at :${config.PORT}`);
 };
