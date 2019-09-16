5050215dd6a49a6f030173a81c0294691729678b
jitendra1405 <53037241+jitendra1405@users.noreply.github.com>
2019-09-16 13:41:05 +0530

    Update server.js

--- server/lib/server.js
+++ server/lib/server.js
@@ -4,10 +4,30 @@ const socket = require('./socket');
 
 const app = express();
 const server = createServer(app);
+var mysql = require('mysql');
 
 
 app.use('/', express.static(`${process.cwd()}/../client`));
+var db_config = {
+    host     : 'sql12.freemysqlhosting.net',
+    database : 'sql12304794',
+    user     : 'sql12304794',
+    password : 'PLSEEGHnWv'
+};
 
+var connection;
+
+connection = mysql.createConnection(db_config);
+app.get('/', function(request, response) {
+    connection.query('SELECT * from user', function(err, rows, fields) {
+        if (err) {
+            console.log('error: ', err);
+            throw err;
+        }
+	    
+        response.send(['Hello World!!!! HOLA MUNDO!!!!', rows]);
+    });
+});
 
 
 
