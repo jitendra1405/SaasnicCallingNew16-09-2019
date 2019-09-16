fe91fab19f942529420abaada5b11464bff659a3
jitendra1405 <53037241+jitendra1405@users.noreply.github.com>
2019-09-16 13:29:39 +0530

    Update server.js

--- server/lib/server.js
+++ server/lib/server.js
@@ -8,24 +8,7 @@ const server = createServer(app);
 
 app.use('/', express.static(`${process.cwd()}/../client`));
 
-var mysql = require('mysql');
-var connectDB = {};
 
-  connectDB.connection = mysql.createConnection({
-    host     : 'sql12.freemysqlhosting.net',
-    database : 'sql12304794',
-    user     : 'sql12304794',
-    password : 'PLSEEGHnWv'
-  });
-
-  connectDB.connection.connect(function(err) {
-    if (err) {
-      console.error('error connecting: ' + err.stack);
-      return;
-    }
-
-    console.log('connected as id ' + connectDB.connection.threadId);
-  });
 
 
 
@@ -36,4 +19,3 @@ module.exports.run = (config) => {
   socket(server);
   console.log(`Server is listening at :${config.PORT}`);
 };
-module.exports = connectDB;
