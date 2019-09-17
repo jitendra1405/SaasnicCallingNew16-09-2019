aa520312dd9dc588a720edc9f29b2a0320a129c1
jitendra1405 <53037241+jitendra1405@users.noreply.github.com>
2019-09-17 14:38:59 +0530

    Update haiku.js

--- server/lib/haiku.js
+++ server/lib/haiku.js
@@ -45,23 +45,6 @@ function setValue(value) {
   someVar = value[0].UserName;
   console.log('nnnnnnnnnnnnnnnnnnnnnnn',someVar);
 }
-const bodyParser = require("body-parser");
-const express = require("express");
-const app = express();
-
-app.use(bodyParser.urlencoded({ extended: false }));
-
-app.get('/', (req, res) => {
-  res.sendFile(`${__dirname}/index.html`);
-});
-
-app.post('/login', (req, res) => {
-  const username = req.body.username;
-  const password = req.body.password;
-  console.log(`POST request: username is ${username} and password is ${password}`);
-  res.end(`You are now logged in Mr(s) ${username}`);
-  console.log('username is the ',username);
-});
 
 
 
