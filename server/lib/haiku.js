fb82510de6129dd9a262a82fe934fe75d5066955
jitendra1405 <53037241+jitendra1405@users.noreply.github.com>
2019-09-17 12:00:12 +0530

    Update haiku.js

--- server/lib/haiku.js
+++ server/lib/haiku.js
@@ -47,7 +47,7 @@ function setValue(value) {
 
 var express = require('express'); 
 var app = express(); 
-app.use(express.static('./client/src/html'));
+app.use(express.static('./client'));
 app.get('/myform', function(req, res){ 
     var myText = req.query.mytext; //mytext is the name of your input box
     res.send('Your Text:' +myText); 
