369aad87c9c6f087123e9de17d5dac6f329be59d
jitendra1405 <53037241+jitendra1405@users.noreply.github.com>
2019-09-17 15:59:25 +0530

    Update server.js

--- server/lib/server.js
+++ server/lib/server.js
@@ -1,36 +1,15 @@
-const express = require('express');
+/*const express = require('express');
 const { createServer } = require('http');
 const socket = require('./socket');
 
 const app = express();
 const server = createServer(app);
-const bodyParser = require("body-parser");
-
-//const express = require("express");
-//const app = express();
-app.use('/', express.static(`${process.cwd()}/../client`));
-app.use(bodyParser.urlencoded({ extended: false }));
-var abcde;
-app.get('/', (req, res) => {
-  res.sendFile(`${__dirname}/index.html`);
-});
-
-app.post('/login', (req, res) => {
-  const username = req.body.username;
-  const password = req.body.password;
-  console.log(`POST request: username is ${username} and password is ${password}`);
-  res.end(`You are now logged in Mr(s) ${username}`);
-  console.log('username is the ',username);
-  abcde = username;
-  module.exports.variableName = "variableValue";
-});
 
 
-console.log('abcdeddedede', abcde);
 
 
 module.exports.run = (config) => {
   server.listen(config.PORT);
   socket(server);
   console.log(`Server is listening at :${config.PORT}`);
-};
+};*/
