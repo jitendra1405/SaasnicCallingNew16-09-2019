c11301cedb8de5d5103a771c306b9e0d75ea3326
jitendra1405 <53037241+jitendra1405@users.noreply.github.com>
2019-09-17 10:42:35 +0530

    Update haiku.js

--- server/lib/haiku.js
+++ server/lib/haiku.js
@@ -23,59 +23,6 @@ const nouns = [
 ];
 var someVar = [];
 
-
-var express = require('express');
-var app = express();
-var bodyParser = require('body-parser');
-
-// set ejs as rendering engine
-app.set('view engine', 'ejs');
-
-// parse html forms
-app.use(bodyParser.urlencoded({ extended : false }));
-
-// render the ejs page
-app.get('/', function (req, res) {
-  res.render('index.ejs');
-});
-
-// when Add to Top button is clicked
-app.post('/top', function (req, res) {
-  console.log(req.body.todo + " is added to top of the list.");
-  res.redirect('/');
-});
-
-// when Add to Bottom button is clicked
-app.post('/bottom', function (req, res) {
-  console.log(req.body.todo + " is added to bottom of the list.");
-  res.redirect('/');
-});
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 var mysql = require('mysql');  
 var con = mysql.createConnection({  
   host     : 'sql12.freemysqlhosting.net',
@@ -97,10 +44,6 @@ function setValue(value) {
   someVar = value[0].UserName;
   console.log('nnnnnnnnnnnnnnnnnnnnnnn',someVar);
 }
-
-
-
-
 
 module.exports = () => {
   
