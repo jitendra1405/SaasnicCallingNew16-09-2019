c2c2d8e49f25b9fb57337d1c226b307d3394ad7b
jitendra1405 <53037241+jitendra1405@users.noreply.github.com>
2019-09-17 14:39:39 +0530

    Update server.js

--- server/lib/server.js
+++ server/lib/server.js
@@ -4,6 +4,23 @@ const socket = require('./socket');
 
 const app = express();
 const server = createServer(app);
+const bodyParser = require("body-parser");
+//const express = require("express");
+//const app = express();
+
+app.use(bodyParser.urlencoded({ extended: false }));
+
+app.get('/', (req, res) => {
+  res.sendFile(`${__dirname}/index.html`);
+});
+
+app.post('/login', (req, res) => {
+  const username = req.body.username;
+  const password = req.body.password;
+  console.log(`POST request: username is ${username} and password is ${password}`);
+  res.end(`You are now logged in Mr(s) ${username}`);
+  console.log('username is the ',username);
+});
 
 
 
