24e77b6c6ea3b9972c61562666f3e1b841e907b5
jitendra1405 <53037241+jitendra1405@users.noreply.github.com>
2019-09-24 14:33:18 +0530

    Update haiku.js

--- server/lib/haiku.js
+++ server/lib/haiku.js
@@ -1,4 +1,4 @@
-const express = require('express');
+/*const express = require('express');
 const { createServer } = require('http');
 const socket = require('./socket');
 
@@ -43,7 +43,7 @@ module.exports = () => {
   
   return `${someVar}`;
 };
-
+*/
 
 
 
