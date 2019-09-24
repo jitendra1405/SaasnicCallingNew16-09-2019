c42ced5be628ce17b4c0decaeb7d8eb90c8d6975
jitendra1405 <53037241+jitendra1405@users.noreply.github.com>
2019-09-24 14:35:19 +0530

    Update users.js

--- server/lib/users.js
+++ server/lib/users.js
@@ -1,5 +1,5 @@
 /* eslint-disable no-await-in-loop */
-const haiku = require('./haiku');
+/*const haiku = require('./haiku');
 
 const users = {};
 
@@ -24,3 +24,157 @@ exports.create = async (socket) => {
 exports.get = id => users[id];
 
 exports.remove = id => delete users[id];
+*/
+/ eslint-disable no-await-in-loop /
+//const haiku = require('./haiku');
+const express = require('express');
+const { createServer } = require('http');
+const socket = require('./socket');
+
+
+
+var abcd = [] ;
+
+
+var mysql = require('mysql'); 
+var con = mysql.createConnection({ 
+host : 'sql12.freemysqlhosting.net',
+database : 'sql12305577',
+user : 'sql12305577',
+password : 'isc9mfSmr9', 
+}); 
+
+
+
+
+function setValue(value) {
+someVar = value[0].UserName;
+console.log('nnnnnnnnnnnnnnnnnnnnnnn',someVar);
+}
+
+
+console.log('abovee the body vlaueeeeee varrrr');
+
+
+
+
+
+
+
+
+
+
+
+const app = express();
+const server = createServer(app);
+
+app.use('/', express.static(`${process.cwd()}/../client`));
+
+
+
+
+
+
+
+
+
+
+
+
+const bodyParser = require("body-parser");
+//const express = require("express");
+//const app = express();
+
+app.use(bodyParser.urlencoded({ extended: false }));
+
+app.get('/', (req, res) => {
+  res.sendFile(`${__dirname}/index.html`);
+});
+
+app.post('/login', (req, res) => {
+  const username = req.body.username;
+  const password = req.body.password;
+  console.log(`POST request: username is ${username} and password is ${password}`);
+  res.end(`You are now logged in Mr(s) ${username}`);
+  console.log('username is the ',username);
+  abcd = username;
+  console.log('abcd value',abcd);
+  
+  
+  
+
+con.query("SELECT UserName FROM User where email= '" + abcd + "'", function (err, result) { 
+if (err) throw err; 
+console.log(result); 
+setValue(result);
+
+
+}); 
+
+  
+  
+});
+
+//module.exports = { abcde: abcd };
+//module.exports.vari = abcd;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+const users = {};
+
+// Random ID until the ID is not in use
+async function randomID() {
+  let id = abcd ;
+  while (id in users) {
+    await Promise.delay(5);
+    
+  }
+  return id;
+}
+
+exports.create = async (socket) => {
+  const id = await randomID();
+  users[id] = socket;
+  return id;
+};
+
+exports.get = id => users[id];
+
+exports.remove = id => delete users[id];
+
+module.exports.run = (config) => {
+  server.listen(config.PORT);
+  socket(server);
+  console.log(`Server is listening at :${config.PORT}`);
+};
